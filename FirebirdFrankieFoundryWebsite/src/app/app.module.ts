4c313dddb22f19435726faaad827c367d6fe6605
Renton McIntyre <rentonmcintyre@yahoo.com>
2020-12-17 21:39:52 +0200

    Added high quality SVGs and used SVG for top-right logo

--- FirebirdFrankieFoundryWebsite/src/app/app.module.ts
+++ FirebirdFrankieFoundryWebsite/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card'
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
