968e958ab3304186b1ab61e750306e9425d90e3c
Renton McIntyre <rentonmcintyre@yahoo.com>
2020-12-20 17:23:52 +0200

    GalleryComponent aesthetic completed

--- FirebirdFrankieFoundryWebsite/src/app/gallery/gallery.component.ts
+++ FirebirdFrankieFoundryWebsite/src/app/gallery/gallery.component.ts
@@ -6,21 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  expandedP1: boolean = false;
-  expandedP2: boolean = false;
-  expandedP3: boolean = false;
-
+  expanded: boolean[] = [ false, false, false ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
   expandBlock(id: number): void {
-    if (id == 1)
-      this.expandedP1 = !this.expandedP1;
-    if (id == 2)
-      this.expandedP2 = !this.expandedP2;
-    if (id == 3)
-      this.expandedP3 = !this.expandedP3;
+    this.expanded[id] = !this.expanded[id];
   }
 }
