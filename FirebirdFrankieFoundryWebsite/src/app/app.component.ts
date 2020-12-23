ba50fc07c0a3aaee62b27d199d58c764f16a6b90
Firebird Frankie <firebirdfrankiefoundry@gmail.com>
2020-12-23 13:37:01 +0200

    Toolbar added with content and hyperlink to dev LinkedIn. Various minor padding adjustments, etc.

--- FirebirdFrankieFoundryWebsite/src/app/app.component.ts
+++ FirebirdFrankieFoundryWebsite/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  currentYear: number = (new Date()).getFullYear();  
+
   constructor(private router: Router ) {
   }
 
